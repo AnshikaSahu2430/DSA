11a553e6a74dbbf289b014ecd643d437eefbe627
Anshika Sahu <anshikasahu2430@gmail.com>
2025-08-25 15:11:43 +0530

    Arrays added

---
+++ ARRAYS/missingrepeated.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class missingrepeated {
+    public static void main(String args[]){
+        int grid[][]={{1,3},{2,2}};
+        missing(grid);
+    }
+    public static void missing(int grid[]){
+        int r=grid.length;
+        int element=grid([0][0]);
+        for(int i=1;i<r;i++){
+            
+                grid[i]==
+            
+        }
+    }
+}
